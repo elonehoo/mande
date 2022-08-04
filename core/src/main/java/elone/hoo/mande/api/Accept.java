b01fed9969f07e4dfa9e33c9772ebd4e211bdbdb
xiaoxunyao <huchengyea@163.com>
2022-08-04 09:12:15 +0800

    fix: improve the po module of the entity class

--- core/src/main/java/elone/hoo/mande/api/Accept.java
+++ core/src/main/java/elone/hoo/mande/api/Accept.java
@@ -1,17 +1,8 @@
 package elone.hoo.mande.api;
 
-import com.restful.Result;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/accept")
 public class Accept {
-
-  @GetMapping("/hello")
-  public Result hello() {
-    return Result.success("hello");
-  }
-
 }
