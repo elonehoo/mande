06696e8085717364e88c85547a0ab149b244016e
xiaoxunyao <huchengyea@163.com>
2022-08-08 17:46:44 +0800

    chore: init lib module

---
+++ lib/src/test/java/elone/hoo/lib/LibApplicationTests.java
@@ -0,0 +1,13 @@
+package elone.hoo.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
