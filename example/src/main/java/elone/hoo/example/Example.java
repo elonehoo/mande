df56d6ba05d998790223e13b4b539356c5d54d86
xiaoxunyao <huchengyea@163.com>
2022-08-10 11:21:45 +0800

    example: init example

---
+++ example/src/main/java/elone/hoo/example/Example.java
@@ -0,0 +1,13 @@
+package elone.hoo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Example.class, args);
+  }
+
+}
